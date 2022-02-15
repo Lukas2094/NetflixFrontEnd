022ab6655a3d3b020875df16dbeb361475e56ead
lucassilvaativo <lucas.silva@ativo.com>
2022-02-15 16:59:09 -0300

    user logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ export default ({black}) => {
             </div>
             <div className="header--user">
                  <a href="/">
-                     <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="User"/>
+                     <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117" alt="User"/>
                  </a>
             </div>
         </header>
